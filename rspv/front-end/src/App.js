6e7d4de6ac580ba199205455546e7fe4c80df3e3
TCG <ssibonokuhle@gmail.com>
2020-11-05 12:27:37 +0200

    Added a file if index.js

--- rspv/front-end/src/App.js
+++ rspv/front-end/src/App.js
@@ -1,29 +1,12 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Router, Redirect, Switch } from 'react-router-dom';
+import React from "react"
+import Form from "./form"
 
-import AuthPage from './pages/Auth';
-import BookingsPage from './pages/Bookings';
-import EventsPage from './pages/Events';
-
-import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-
-      
-      <BrowserRouter>
-      <Switch>
-      <Redirect from="/" to="/autho" exact />
-      <Router path ="/auth" component ={AuthoPage} />
-      <Route path="/events" Component = {Bookings} />
-      <Route path="/bookings" Component = {Events} />
-      </Switch>
-      </BrowserRouter>
-  
-
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
 }
 
 export default App;
--- rspv/front-end/src/pages/Events.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-
-class EventPage extends Component {
-    render (){
-        return <h1>The Events Page</h1>;
-    }
-}
-export default EventsPage;
