9f138e1a7443f98a02d96c6c169684112eed77e2
TCG <ssibonokuhle@gmail.com>
2020-11-06 13:23:27 +0200

    Changed the code, I use Schema

--- rspv/back-end/model/user.js
+++ rspv/back-end/model/user.js
@@ -1,9 +1,16 @@
 const mongoose = require("mongoose");
 
-
-return (
-    <tr>
-      <td>{name}</td>
-      <td>{Surname}</td>
-    </tr>
+const userSchema = new mongoose.Schema({
+    Name: {type: String, required: true},
+    Surname: {type: String, required: true},
+    Email: {type: String, required: true},
+    Dietary: {type: String, required: true },
+})
+// class ProductCategoryRow extends React.Component {
+//     render() {
+//return (
+//     <tr>
+//       <td>{name}</td>
+//       <td>{Surname}</td>
+//     </tr>
 module.exports = mongoose.model ("User", userSchema);
