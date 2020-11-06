ee98d2f0edc397c4cf63105f4341a82b1b6bf51f
TCG <ssibonokuhle@gmail.com>
2020-11-06 13:03:21 +0200

    const mongoose

--- rspv/back-end/model/user.js
+++ rspv/back-end/model/user.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    Name: {type: String, required: true ,trim: true},
-    Surname: {type: String, required: true, trim: true},
-    Email: {type: String, required: true, trim: true},
-    Dietary: {type: String, required: true, trim: true},
+    Name: {type: String, required: true},
+    Surname: {type: String, required: true},
+    Email: {type: String, required: true},
+    Dietary: {type: String, required: true },
 })
 
 module.exports = mongoose.model ("User", userSchema);
