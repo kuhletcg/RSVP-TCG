8ede4cea901c6e181dec853264acc63d0d0f3053
TCG <ssibonokuhle@gmail.com>
2020-11-06 13:39:31 +0200

    Added description in line 4

--- rspv/front-end/src/events/objects.js
+++ rspv/front-end/src/events/objects.js
@@ -1,7 +1,7 @@
  const events = [
      {id: 0, Name:"Zoo Lake", Description: "Zoo Lake is a lovely to go for a walk with a beautiful and well-kept space, lots of birds and one can go row boats on the lake", Date:"01 December"},
 
-    {id: 0, Name:"Lone hill", Description:"Zoo Lake is a lovely park to go for a walk with a beautiful and well-kelp space, lots of birds one can go row boats on the lake", Date: "27 November 2020"},
+    {id: 0, Name:"Lone hill", Description:"Lonehill Nature Reserve is fenced and locked. The reserve is unlocked on weekends, and you can hike to the top where you can picnic, or just lay out a picnic blanket on the surrounding grassy areas.", Date: "27 November 2020"},
  {id: o, Name:"Joburg Zoo", Description: "Johannesburg’s zoo is home to 2070 animals and 365 different species, and without doubt the reason one visits this beautiful setting is the animals themselves.", Date: "on Surtadays"}
 ]
  events.map (events =>  <div key = {events.id}>
