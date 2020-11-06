d952bfe46c7f5fd4957e5598204b2d676a6e74ad
TCG <ssibonokuhle@gmail.com>
2020-11-06 13:17:09 +0200

    added model.export

--- rspv/back-end/model/user.js
+++ rspv/back-end/model/user.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    Name: {type: String, required: true},
-    Surname: {type: String, required: true},
-    Email: {type: String, required: true},
-    Dietary: {type: String, required: true },
-})
 
+return (
+    <tr>
+      <td>{name}</td>
+      <td>{Surname}</td>
+    </tr>
 module.exports = mongoose.model ("User", userSchema);
