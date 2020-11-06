1331aaead63d47ad651b864a72f71569cfef542e
TCG <ssibonokuhle@gmail.com>
2020-11-06 12:34:42 +0200

    uncommend out BrowserRouter

--- rspv/back-end/Route/user.js
+++ rspv/back-end/Route/user.js
@@ -1,4 +1,30 @@
+
 const User = require('../model/user')
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./App";
+// import { BrowserRouter } from "react-router-dom";
+
+// ReactDOM.render(
+//   <BrowserRouter>
+
+
+
+//     <App />
+//   </BrowserRouter>,
+//   document.getElementById("root")
+// );
+// console.log("route")
+//         const { name, surname, email, dietaryRequirements } = req.body
+//         const user = new User ({
+//             name: name,
+//             surname: surname,
+//             email: email,
+//             dietaryRequirements: dietaryRequirements,
+//         })
+// try {
+
 
 const route = (app) => {
     app.post("/book", async(req, res)  => {
